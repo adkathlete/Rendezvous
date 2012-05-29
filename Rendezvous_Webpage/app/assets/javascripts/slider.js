8071a4f680ca4610d4348f123e8311d2f1eca677
Your Name <dennizk@gmail.com>
2012-05-29 03:08:22 -0700

    Website looking good on a hi-res Mac screen. Need to fix the background and add a couple of pages (which is really easy because the CSS is done at this point

--- Rendezvous_Webpage/app/assets/javascripts/slider.js
+++ Rendezvous_Webpage/app/assets/javascripts/slider.js
@@ -22,10 +22,16 @@ function Slider(iphone1, iphone2, iphone3, iphone4, iphone5, advance, text_conte
 Slider.prototype.generate_content = function()
 {
 
-	var list = document.createElement("UL");
-	this.text_content.appendChild(list);
-	list.className = "jcarousel-skin-name";
-	list.id = "mainContent";
+	this.list = document.createElement("UL");
+	this.list.style.left = "-2060px";
+	this.list.style.position = "relative";
+	this.list.style.width = "14420px";
+	this.list.style.zIndex ="39";
+	this.list.style.marginTop = "100px";
+
+	this.text_content.appendChild(this.list);
+	this.list.className = "jcarousel-skin-name";
+	this.list.id = "mainContent";
 
 	var titleTexts = ["Meet Rendezvous", "Just for you", "Smart", "Better with friends", "Find your Rendezvous"];
 	var contentTexts = 	["The first dating app that lets you connect with friends you were too shy to ask out",
@@ -34,7 +40,7 @@ Slider.prototype.generate_content = function()
 						"The more of your friends use Rendezvous, the better the matching gets and the more you get a chance of meeting your dream girl.", 
 						"It's free for the iPhone and any device running iOS 4.0 or higher.<br/>Find your Rendezvous, wherever he/she is."];
 
-	for(var i =1; i<5; i++)
+	for(var i =0; i<5; i++)
 	{
 		var currentListNode = document.createElement("LI");
 		var currentTitleText = document.createTextNode(titleTexts[i]);
@@ -51,7 +57,7 @@ Slider.prototype.generate_content = function()
 		wrapper.appendChild(currentParagraph);
 		currentParagraph.appendChild(currentParagraphText);
 
-		list.appendChild(currentListNode);
+		this.list.appendChild(currentListNode);
 		currentListNode.style.overflow =  "hidden";
 		currentListNode.style.cssFloat = "left"; 
 		currentListNode.style.width = "2000px";
@@ -62,6 +68,7 @@ Slider.prototype.generate_content = function()
 	iphoneAppLink.href = "http://itunes.apple.com/";
 	var app_text = document.createElement("SPAN");
 	app_text.innerHTML = "Get the app";
+	iphoneAppLink.style.top = "450px";
 
 	this.text_content.appendChild(iphoneAppLink);
 }
@@ -75,30 +82,35 @@ Slider.prototype.advanceAction = function()
 	  this.currentActive = 2;
 	  this.iphone2.className = "phone active";
 	  this.iphone1.className = "phone";
+	  this.list.style.left = "-4120px";
 	  break;
 	case 2:
 	window.console.log("Switching to 3. currentActive is: "+this.currentActive);
 	  this.currentActive = 3;
 	  this.iphone3.className = "phone active";
 	  this.iphone2.className = "phone";
+	  this.list.style.left = "-6180px";
 	  break;
 	case 3:
 	window.console.log("Switching to 4. currentActive is: "+this.currentActive);
 	  this.currentActive = 4;
 	  this.iphone4.className = "phone active";
 	  this.iphone3.className = "phone";
+	  this.list.style.left = "-8240px";
 	  break;
 	case 4:
 	window.console.log("Switching to 5. currentActive is: "+this.currentActive);
 	  this.currentActive = 5;
 	  this.iphone5.className = "phone active";
 	  this.iphone4.className = "phone";
+	  this.list.style.left = "-10300px";
 	  break;
 	case 5:
 	window.console.log("Switching to 1. currentActive is: "+this.currentActive);
 	  this.currentActive = 1;
 	  this.iphone1.className = "phone active";
 	  this.iphone5.className = "phone";
+	  this.list.style.left = "-2060px";
 	  break;
 	default:
 	  window.console.log("Something is wrong");
